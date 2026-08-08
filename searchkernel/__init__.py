53fb5a2cfd2615826d5b974c2b35940794cd84b6
semantic-release <semantic-release>
2026-08-08 22:36:16 +0000

    chore(release): 0.20.0

--- searchkernel/__init__.py
+++ searchkernel/__init__.py
@@ -96,7 +96,7 @@ from searchkernel.search.record_pipeline import (
 )
 from searchkernel.search.utils import classify_query_type, truncate_content
 
-__version__ = "0.19.0"
+__version__ = "0.20.0"
 
 __all__ = [
     "FEDERATION_CONTRACT_VERSION",
